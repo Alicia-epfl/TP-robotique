b2052599f4f2e15d4efd205a3a0588f7714b4b8a
Alicia-epfl <alicia.mauroux@epfl.ch>
2021-03-31 10:05:05 +0200

    Task 4

--- TP4_CamReg/CamReg/process_image.h
+++ TP4_CamReg/CamReg/process_image.h
@@ -1,6 +1,7 @@
 #ifndef PROCESS_IMAGE_H
 #define PROCESS_IMAGE_H
 
+//pas besoin de mettre extract_line car utilisée que dans processImage :)
 float get_distance_cm(void);
 void process_image_start(void);
 
