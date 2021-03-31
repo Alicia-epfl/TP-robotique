c188779a5d38fa18146c1baab728733e968af76f
Nicolas <Nicolas.nouel@epfl.ch>
2021-03-31 10:46:03 +0200

    Get_line_position

--- TP4_CamReg/CamReg/process_image.c
+++ TP4_CamReg/CamReg/process_image.c
@@ -220,6 +220,11 @@ float get_distance_cm(void){
 	return distance_cm;
 }
 
+uint16_t get_line_position(void){
+	return line_position;
+}
+
+
 void process_image_start(void){
 	chThdCreateStatic(waProcessImage, sizeof(waProcessImage), NORMALPRIO, ProcessImage, NULL);
 	chThdCreateStatic(waCaptureImage, sizeof(waCaptureImage), NORMALPRIO, CaptureImage, NULL);
