a222c8064c8bcc27f861da758d8170b7d61b4d25
Alicia-epfl <alicia.mauroux@epfl.ch>
2021-04-20 10:34:18 +0200

    Projet_color

---
+++ Projet_Alicia/CamReg/process_image.h
@@ -0,0 +1,9 @@
+#ifndef PROCESS_IMAGE_H
+#define PROCESS_IMAGE_H
+
+//pas besoin de mettre extract_line car utilisée que dans processImage :)
+float get_distance_cm(void);
+uint16_t get_line_position(void);
+void process_image_start(void);
+
+#endif /* PROCESS_IMAGE_H */
