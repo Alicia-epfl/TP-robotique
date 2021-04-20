e72758e57402023cdccdd7dec7fcfaeebe732652
Alicia-epfl <alicia.mauroux@epfl.ch>
2021-04-20 10:22:21 +0200

    Projet_final

--- projet_Alicia_v1/process_image.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROCESS_IMAGE_H
-#define PROCESS_IMAGE_H
-
-float get_distance_cm(void);
-void find_color(uint8_t *buffer);
-uint16_t get_line_position(void);
-void process_image_start(void);
-
-#endif /* PROCESS_IMAGE_H */
