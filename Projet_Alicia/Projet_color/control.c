58a18bff8341a7eccbffefd0cd9e7f048a373716
Nicolas <Nicolas.nouel@epfl.ch>
2021-05-04 17:44:50 +0200

    control files

---
+++ Projet_Alicia/Projet_color/control.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "ch.h"
+#include "hal.h"
+#include <usbcfg.h>
+#include <chprintf.h>
+#include "sensors/proximity.h"
+#include <main.h>
+#include <proximity_detection.h>
+
+#include <sensors/proximity.h>
+#include <msgbus/messagebus.h>
+
+#include <leds.h>
+
+#include <stdbool.h>
---
+++ Projet_Alicia/Projet_color/control.h
@@ -0,0 +1,15 @@
+/*
+ * led_control.h
+ *
+ *  Created on: May 4, 2021
+ *      Author: Nico
+ */
+
+#ifndef CONTROL_H_
+#define CONTROL_H_
+
+
+
+
+
+#endif /* CONTROL_H_ */
