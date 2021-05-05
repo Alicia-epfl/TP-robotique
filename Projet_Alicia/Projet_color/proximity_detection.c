16da4f403b28b86e616a415cca53ceb9e86207c4
Nicolas <Nicolas.nouel@epfl.ch>
2021-05-05 18:36:17 +0200

    gestion des obstacles avec corrections de direction

--- Projet_Alicia/Projet_color/proximity_detection.c
+++ Projet_Alicia/Projet_color/proximity_detection.c
@@ -9,12 +9,15 @@
 
 #include <sensors/proximity.h>
 #include <msgbus/messagebus.h>
-
+#include <motors.h>
 #include <leds.h>
 
 #include <stdbool.h>
 
-
+/*
+ * lignes où ajouter la gestion de quarts de tours: 193, 203, 216, 226, 241, 250
+ *
+ * */
 
 static bool prox_detected = false;
 static bool prox_front = false;
@@ -23,7 +26,10 @@ static bool prox_right_full = false; // capteur de droite 90°
 static bool prox_left_half = false;  // capteur gauche 45°
 static bool prox_left_full = false;  // capteur gauche 90°
 static bool prox_back = false;
-static int simple_dodge = 0; // faire des defines associés. Sert a enregistrer que le robot a tourné à cause d'un obstacle et le faire retourner dans sa direction initiale dès que possible
+
+static int turn = 0;
+
+//static int simple_dodge = 0; // faire des defines associés. Sert a enregistrer que le robot a tourné à cause d'un obstacle et le faire retourner dans sa direction initiale dès que possible
 /*
  * THREADS
  */
@@ -77,10 +83,175 @@ static THD_FUNCTION(Proximity, arg) {
 		 * SI c'est tout cassé, c'est très certainement ici que ça se passe
 		 * */
 
-		if((get_prox(0)>AXIS_THRESHOLD) || (get_prox(7)>AXIS_THRESHOLD))
+//		if((get_prox(0)>AXIS_THRESHOLD) || (get_prox(7)>AXIS_THRESHOLD))
+//		{
+//			prox_front = true;
+//			set_led(LED1, ON);    // n'est pas destiné à rester là, tout les bool auront leur propre fonction get_bool_prox
+//		}
+//		else
+//		{
+//			set_led(LED1, OFF);
+//			prox_front = false;
+//		}
+//
+//		if(get_prox(1)>THRESHOLD)
+//		{
+//			prox_right_half = true;
+//			set_led(LED1, ON);
+//			set_led(LED3, ON);
+//		}
+//		else
+//		{
+//			set_led(LED1, OFF);
+//			set_led(LED3, OFF);
+//			prox_right_half = false;
+//		}
+//
+//		if(get_prox(2)>AXIS_THRESHOLD)
+//		{
+//			prox_right_full = true;
+//			set_led(LED3, ON);
+//		}
+//		else
+//		{
+//			set_led(LED3, OFF);
+//			prox_right_full = false;
+//		}
+//
+//		if((get_prox(3)>AXIS_THRESHOLD) || (get_prox(4)>AXIS_THRESHOLD))
+//		{
+//			prox_back = true;
+//			set_led(LED5, ON);
+//		}
+//		else
+//		{
+//			prox_back = false;
+//			set_led(LED5, OFF);
+//		}
+//
+//		if(get_prox(6)>THRESHOLD)
+//		{
+//			prox_left_half = true;
+//			set_led(LED1, ON);
+//			set_led(LED7, ON); // c'est peut etre plutot la led 5, je sais plus si elle sont dans le sens horaire ou antihoraire, tant que y'en a deux qui s'allume c'est que ça marche donc osef
+//		}
+//		else
+//		{
+//			set_led(LED1, OFF);
+//			set_led(LED7, OFF);
+//			prox_left_half = false;
+//		}
+//		if(get_prox(5)>AXIS_THRESHOLD)
+//		{
+//			prox_left_full = true;
+//			set_led(LED7, ON);
+//		}
+//		else
+//		{
+//			set_led(LED7, OFF);
+//			prox_left_full = false;
+//		}
+
+
+		/*Fin de si c'est tout cassé*/
+
+
+
+		/*SI c'est tout cassé le retour*/
+		if((get_prox(0)>AXIS_THRESHOLD) || (get_prox(7)>AXIS_THRESHOLD) /* || blue*/)
 		{
-			prox_front = true;
-			set_led(LED1, ON);    // n'est pas destiné à rester là, tout les bool auront leur propre fonction get_bool_prox
+			prox_front = true;  // finalement, n'est pas très utile je crois rip
+			right_motor_set_speed(0); //le robot s'arrete avant de chercher son chemin
+			left_motor_set_speed(0);
+			set_led(LED1, ON);// n'est pas destiné à rester là je pense
+
+
+			//pour l'instant je me limite aux axes principaux pour plus de façilité
+
+			if(get_prox(2)>AXIS_THRESHOLD)//obstacle à droite
+			{
+				prox_right_full = true; //useless pour l'instant
+				set_led(LED3, ON);
+
+				if(get_prox(5)>AXIS_THRESHOLD)//obstacle à gauche
+				{
+					prox_left_full = true; //useless pour l'instant
+					set_led(LED7, ON);
+
+					if((get_prox(3)>AXIS_THRESHOLD) || (get_prox(4)>AXIS_THRESHOLD))//obstacle derrière == robot encerclé
+					{
+						prox_back = true; //useless pour l'instant
+						set_led(LED5, ON);
+					}
+					else // la voie est libre derrière le robot, il se retourne pour progresser dans le parcours
+					{
+						right_motor_set_speed(-100); //le robot se retourne pour trouver une sortie
+						left_motor_set_speed(100);   // tourne de 180° vers la droite == 2 virages à droite pour l'instant
+
+						// manque la gestion du demi tour parfait
+
+//						right_motor_set_speed(100);
+//						left_motor_set_speed(100);
+						turn++;//enregistre un quart de tour vers la droite
+						turn++;
+					}
+				}
+				else //voie est libre à gauche, pas besoin de vérifier derrière, tourne à gauche
+				{
+					right_motor_set_speed(100);
+					left_motor_set_speed(-100);
+					//ajouter gestion durée quart de tour
+//					right_motor_set_speed(100);
+//					left_motor_set_speed(100);
+					turn--;
+				}
+			}
+			else if(turn>0) //turn > 0 == virage à droite non compensé
+			{
+				if(get_prox(5)<AXIS_THRESHOLD) // si y'a pas d'obstacle à gauche, tourner à gauche
+					// peut être finalement se mettre à utiliser mes ptits booleens, appeler une fonction à chaque fois ça pèse un peu plus lourd qu'appeler un booleen non ?
+				{
+					right_motor_set_speed(100);
+					left_motor_set_speed(-100);
+					//ajouter gestion durée quart de tour
+					//right_motor_set_speed(100);
+					//left_motor_set_speed(100);
+					turn--;
+				}
+			}
+			else
+			{
+				right_motor_set_speed(-100);
+				left_motor_set_speed(100);
+				//ajouter gestion durée quart de tour
+				//right_motor_set_speed(100);
+				//left_motor_set_speed(100);
+				turn++;
+			}
+
+
+
+		}
+		else if(((get_prox(2) < AXIS_THRESHOLD) || get_prox(5)<AXIS_THRESHOLD) &&(turn != 0)) // si la voie est libre devant et sur au moins un côté et qu'il y a des virages à compenser
+		{
+			if(turn > 0) // s'il y a des virages a droite à compenser
+			{
+				right_motor_set_speed(100);
+				left_motor_set_speed(-100);
+				//ajouter gestion durée quart de tour
+				//right_motor_set_speed(100);
+				//left_motor_set_speed(100);
+				turn--;
+			}
+			else if(turn < 0) // s'il y a des virages à gauche à compenser
+			{
+				right_motor_set_speed(-100);
+				left_motor_set_speed(100);
+				//ajouter gestion durée quart de tour
+				//right_motor_set_speed(100);
+				//left_motor_set_speed(100);
+				turn++;
+			}
 		}
 		else
 		{
@@ -88,66 +259,10 @@ static THD_FUNCTION(Proximity, arg) {
 			prox_front = false;
 		}
 
-		if(get_prox(1)>THRESHOLD)
-		{
-			prox_right_half = true;
-			set_led(LED1, ON);
-			set_led(LED3, ON);
-		}
-		else
-		{
-			set_led(LED1, OFF);
-			set_led(LED3, OFF);
-			prox_right_half = false;
-		}
-
-		if(get_prox(2)>AXIS_THRESHOLD)
-		{
-			prox_right_full = true;
-			set_led(LED3, ON);
-		}
-		else
-		{
-			set_led(LED3, OFF);
-			prox_right_full = false;
-		}
-
-		if((get_prox(3)>AXIS_THRESHOLD) || (get_prox(4)>AXIS_THRESHOLD))
-		{
-			prox_back = true;
-			set_led(LED5, ON);
-		}
-		else
-		{
-			prox_back = false;
-			set_led(LED5, OFF);
-		}
-
-		if(get_prox(6)>THRESHOLD)
-		{
-			prox_left_half = true;
-			set_led(LED5, ON);
-			set_led(LED7, ON); // c'est peut etre plutot la led 5, je sais plus si elle sont dans le sens horaire ou antihoraire, tant que y'en a deux qui s'allume c'est que ça marche donc osef
-		}
-		else
-		{
-			set_led(LED5, OFF);
-			set_led(LED7, OFF);
-			prox_left_half = false;
-		}
-		if(get_prox(5)>AXIS_THRESHOLD)
-		{
-			prox_left_full = true;
-			set_led(LED7, ON);
-		}
-		else
-		{
-			set_led(LED7, OFF);
-			prox_left_full = false;
-		}
 
 
-		/*Fin de si c'est tout cassé*/
+
+		/*===========================*/
 
 		/* Delay of 250 milliseconds.*/
 		chThdSleepMilliseconds(500);
