50a7a6bee7ac19bc12bb0de45d67e31cd0a7923e
Nicolas <Nicolas.nouel@epfl.ch>
2021-05-12 15:08:20 +0200

    Cleanup définitif du son

--- Projet_Alicia/Projet_color/noise_detection.c
+++ Projet_Alicia/Projet_color/noise_detection.c
@@ -15,19 +15,13 @@
 #include <pi_regulator.h>
 #include "process_image.h"
 
-//semaphore
-//static BSEMAPHORE_DECL(sendToComputer_sem, TRUE);//
+
 
 //2 times FFT_SIZE because these arrays contain complex numbers (real + imaginary)
 static float micLeft_cmplx_input[2 * FFT_SIZE];
-//static float micRight_cmplx_input[2 * FFT_SIZE];
-//static float micFront_cmplx_input[2 * FFT_SIZE];
-//static float micBack_cmplx_input[2 * FFT_SIZE];
+
 //Arrays containing the computed magnitude of the complex numbers
 static float micLeft_output[FFT_SIZE];
-//static float micRight_output[FFT_SIZE];
-//static float micFront_output[FFT_SIZE];
-//static float micBack_output[FFT_SIZE];
 
 static uint8_t freq_found = 0;
 static int8_t led_on = false;
@@ -36,24 +30,9 @@ static uint8_t successive_freq_counter = 0;
 #define MIN_VALUE_THRESHOLD	10000
 #define MAX_MIC_INPUT_COUNTER 10
 
-#define MIN_FREQ		10	//we don't analyze before this index to not use resources for nothing
-#define FREQ_FORWARD		16	//150Hz
-//#define FREQ_LEFT		19	//296Hz
-//#define FREQ_RIGHT		23	//359HZ
-//#define FREQ_BACKWARD	26	//406Hz
-#define MAX_FREQ		30	//we don't analyze after this index to not use resources for nothing
 
 #define L_FREQ 90
 #define M_FREQ 110
-
-#define FREQ_FORWARD_L		(FREQ_FORWARD-2)
-#define FREQ_FORWARD_H		(FREQ_FORWARD+2)
-#define FREQ_LEFT_L			(FREQ_LEFT-1)
-#define FREQ_LEFT_H			(FREQ_LEFT+1)
-#define FREQ_RIGHT_L		(FREQ_RIGHT-1)
-#define FREQ_RIGHT_H		(FREQ_RIGHT+1)
-#define FREQ_BACKWARD_L		(FREQ_BACKWARD-1)
-#define FREQ_BACKWARD_H		(FREQ_BACKWARD+1)
 
 /*
 *	Simple function used to detect the highest value in a buffer
@@ -62,7 +41,7 @@ static uint8_t successive_freq_counter = 0;
 void sound_remote(float* data){
 	float max_norm = MIN_VALUE_THRESHOLD;
 	int16_t max_norm_index = -1;
-	uint8_t right =0, left=0;
+	//uint8_t right =0, left=0;
 
 	//search for the highest peak
 	for(uint16_t i = L_FREQ ; i <= M_FREQ ; i++){
@@ -72,27 +51,12 @@ void sound_remote(float* data){
 		}
 	}
 
-	//go forward --> du coup nous on veut plutôt un toggle à chaque fois qu'il entend la fréquence right? En mode on a un static RUN qu'on met à 1 ou 0
-	// et en fonction de sa valeur on dit go forward ou stop right? Et le gauche et droite de ici on l'utilise dans les couleurs
 	if(max_norm_index >= L_FREQ && max_norm_index <= M_FREQ){
-//		left_motor_set_speed(600);
-//		right_motor_set_speed(600);
-
-		//Pour tester le GO and STOP, ça fonctionne avec le "mmmmmh" mais pas avec Go et Stop mdr
 		freq_found = true;
-		//set_body_led(ON);//tests
-
-//		if(run){
-//			run = false;
-//		}else{
-//			run = true;
-////			RUN = false;
-//		}
 	}
 	else
 	{
 		freq_found = false;
-		//set_body_led(OFF);// tests
 	}
 
 }
@@ -117,22 +81,16 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 	*/
 
 	static uint16_t nb_samples = 0;
-	static uint8_t mustSend = 0;
 
 	//loop to fill the buffers
 	for(uint16_t i = 0 ; i < num_samples ; i+=4){
 		//construct an array of complex numbers. Put 0 to the imaginary part
-//		micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
+
 		micLeft_cmplx_input[nb_samples] = (float)data[i + MIC_LEFT];
-//		micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
-//		micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
 
 		nb_samples++;
 
-//		micRight_cmplx_input[nb_samples] = 0;
 		micLeft_cmplx_input[nb_samples] = 0;
-//		micBack_cmplx_input[nb_samples] = 0;
-//		micFront_cmplx_input[nb_samples] = 0;
 
 		nb_samples++;
 
@@ -149,10 +107,8 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 		*	This is an "In Place" function.
 		*/
 
-//		doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
 		doFFT_optimized(FFT_SIZE, micLeft_cmplx_input);
-//		doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
-//		doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
+
 
 		/*	Magnitude processing
 		*
@@ -161,20 +117,10 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 		*	real numbers.
 		*
 		*/
-//		arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
-		arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
-//		arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
-//		arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
 
-		//sends only one FFT result over 10 for 1 mic to not flood the computer
-		//sends to UART3
-//		if(mustSend > 8){
-//			//signals to send the result to the computer
-////			chBSemSignal(&sendToComputer_sem);
-//			mustSend = 0;
-//		}
+		arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
+
 		nb_samples = 0;
-//		mustSend++;
 
 		sound_remote(micLeft_output);
 		if(successive_freq_counter > MAX_MIC_INPUT_COUNTER) //environs 1/2 secondes
@@ -210,31 +156,16 @@ float* get_audio_buffer_ptr(BUFFER_NAME_t name){
 	if(name == LEFT_CMPLX_INPUT){
 		return micLeft_cmplx_input;
 	}
-//	else if (name == RIGHT_CMPLX_INPUT){
-//		return micRight_cmplx_input;
-//	}
-//	else if (name == FRONT_CMPLX_INPUT){
-//		return micFront_cmplx_input;
-//	}
-//	else if (name == BACK_CMPLX_INPUT){
-//		return micBack_cmplx_input;
-//	}
+
 	else if (name == LEFT_OUTPUT){
 		return micLeft_output;
 	}
-//	else if (name == RIGHT_OUTPUT){
-//		return micRight_output;
-//	}
-//	else if (name == FRONT_OUTPUT){
-//		return micFront_output;
-//	}
-//	else if (name == BACK_OUTPUT){
-//		return micBack_output;
-//	}
+
 	else{
 		return NULL;
 	}
 }
+
 uint8_t get_run(void){
 	return freq_found;
 }
